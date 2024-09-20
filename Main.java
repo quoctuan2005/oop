dba7fb416ee085d8ff12f082c9470b3fb1e50cff
quoctuan2005 <144255278+quoctuan2005@users.noreply.github.com>
2024-09-20 15:14:20 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static String printHello() {
+            return "Sơn Ngu";
+    }
+    public static void main(String args[]) {
+        String hello =  printHello();
+        System.out.printf(hello);
+    }
+}
