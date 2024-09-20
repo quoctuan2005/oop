e441c694003eb68e95c29d8eacc4e6000230cf78
quoctuan2005 <144255278+quoctuan2005@users.noreply.github.com>
2024-09-20 15:14:40 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static String printHello() {
-            return "Sơn Ngu";
+            return "HelloWorld";
     }
     public static void main(String args[]) {
         String hello =  printHello();
