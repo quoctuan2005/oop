edee04a7027d25418f1228b9083ba684d937334d
quoctuan2005 <144255278+quoctuan2005@users.noreply.github.com>
2024-09-27 19:55:11 +0700

    vjp

--- Week4 JUnit/Week4.java
+++ Week4 JUnit/Week4.java
@@ -1,31 +1,31 @@
 public class Week4 {
     public static int max2Int(int a, int b) {
-        if (a > b) {
-            return a;
-        }
-        return b;
+        return Math.max(a,b);
     }
-
     public static int minArray (int[] array) {
-        int frag = Integer.MAX_VALUE;
-        for (int i = 0; i < array.length; ++i) {
-            if (array[i] < frag) {
-               frag = array[i];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++){
+            if (array[i] < min) {
+                min = array[i];
             }
         }
-        return frag;
+        return min;
     }
+    public static String calculateBMI(double weight, double height) {
+        double BMI = weight / Math.pow( height, 2);
+        double round = Math.round(BMI * 10.0) / 10.0;
 
-    public static String calculateBMI(double weight, double height){
-        double bmi = weight / (height * height);
-        double lamTron = Math.round(bmi * 10.0) / 10.0;
-        if (lamTron < 18.5) {
+        if (round < 18.5) {
             return "Thiếu cân";
-        } else if (lamTron < 23) {
-            return "Bình thường";
-        } else if (lamTron < 25) {
-            return "Thừa cân";
         }
-        return "Béo phì";
+        else if (round >= 18.5 && round <= 22.9) {
+            return  "Bình thường";
+        }
+        else if (round >= 23 && round <= 24.9) {
+            return  "Thừa cân";
+        }
+        else {
+            return  "Béo phì";
+        }
     }
 }
